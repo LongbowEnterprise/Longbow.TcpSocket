b5d18345b3194aaa99e66e5cd11202c1372011db
Argo Zhang <argo@live.ca>
2025-09-10 12:20:15 +0800

    feat: 增加 Sender 内部类

--- src/Longbow.TcpSocket/DefaultTcpSocketClientProvider.cs
+++ src/Longbow.TcpSocket/DefaultTcpSocketClientProvider.cs
@@ -14,12 +14,13 @@ namespace Longbow.TcpSocket;
 [UnsupportedOSPlatform("browser")]
 sealed class DefaultTcpSocketClientProvider : ITcpSocketClientProvider
 {
-    private TcpClient? _client;
+    private TcpClient? _tcpClient;
+    private Sender? _sender;
 
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
-    public bool IsConnected => _client?.Connected ?? false;
+    public bool IsConnected => _tcpClient?.Connected ?? false;
 
     /// <summary>
     /// <inheritdoc/>
@@ -33,16 +34,18 @@ sealed class DefaultTcpSocketClientProvider : ITcpSocketClientProvider
     {
         await CloseAsync();
 
-        _client = new TcpClient(LocalEndPoint);
-        await _client.ConnectAsync(endPoint, token).ConfigureAwait(false);
-        if (_client.Connected)
+        _tcpClient = new TcpClient(LocalEndPoint);
+        _sender = new Sender(_tcpClient.Client);
+
+        await _tcpClient.ConnectAsync(endPoint, token).ConfigureAwait(false);
+        if (_tcpClient.Connected)
         {
-            if (_client.Client.LocalEndPoint is IPEndPoint localEndPoint)
+            if (_tcpClient.Client.LocalEndPoint is IPEndPoint localEndPoint)
             {
                 LocalEndPoint = localEndPoint;
             }
         }
-        return _client.Connected;
+        return _tcpClient.Connected;
     }
 
     /// <summary>
@@ -51,10 +54,9 @@ sealed class DefaultTcpSocketClientProvider : ITcpSocketClientProvider
     public async ValueTask<bool> SendAsync(ReadOnlyMemory<byte> data, CancellationToken token = default)
     {
         var ret = false;
-        if (_client != null)
+        if (_sender != null)
         {
-            var stream = _client.GetStream();
-            await stream.WriteAsync(data, token).ConfigureAwait(false);
+            await _sender.SendAsync(data, token);
             ret = true;
         }
         return ret;
@@ -66,14 +68,14 @@ sealed class DefaultTcpSocketClientProvider : ITcpSocketClientProvider
     public async ValueTask<int> ReceiveAsync(Memory<byte> buffer, CancellationToken token = default)
     {
         var len = 0;
-        if (_client is { Connected: true })
+        if (_tcpClient is { Connected: true })
         {
-            var stream = _client.GetStream();
+            var stream = _tcpClient.GetStream();
             len = await stream.ReadAsync(buffer, token).ConfigureAwait(false);
 
             if (len == 0)
             {
-                _client.Close();
+                _tcpClient.Close();
             }
         }
         return len;
@@ -84,10 +86,16 @@ sealed class DefaultTcpSocketClientProvider : ITcpSocketClientProvider
     /// </summary>
     public ValueTask CloseAsync()
     {
-        if (_client != null)
+        if (_tcpClient != null)
         {
-            _client.Close();
-            _client = null;
+            _tcpClient.Close();
+            _tcpClient = null;
+        }
+
+        if (_sender != null)
+        {
+            _sender.Dispose();
+            _sender = null;
         }
 
         return ValueTask.CompletedTask;
---
+++ src/Longbow.TcpSocket/Sender.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+using System.Buffers;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace Longbow.TcpSocket;
+
+internal class Sender : IDisposable
+{
+    private readonly SocketAsyncEventArgs _args;
+    private readonly System.Net.Sockets.Socket _socket;
+
+    public Sender(System.Net.Sockets.Socket socket)
+    {
+        _socket = socket;
+        _args = new();
+        _args.Completed += OnSendCompleted;
+    }
+
+    public ValueTask SendAsync(ReadOnlyMemory<byte> data, CancellationToken token = default)
+    {
+        var tcs = new TaskCompletionSource();
+        var state = new SendState();
+        try
+        {
+            if (MemoryMarshal.TryGetArray(data, out var segment))
+            {
+                state.Handle = data.Pin();
+                _args.SetBuffer(segment.Array, segment.Offset, segment.Count);
+            }
+            else
+            {
+                _args.SetBuffer(MemoryMarshal.AsMemory(data));
+            }
+
+            _args.UserToken = (state, tcs);
+
+            if (!_socket.SendAsync(_args))
+            {
+                OnSendCompleted(null, _args);
+            }
+        }
+        catch (Exception ex)
+        {
+            state.Dispose();
+            tcs.SetException(ex);
+        }
+
+        return new ValueTask(tcs.Task);
+    }
+
+    private void OnSendCompleted(object? sender, SocketAsyncEventArgs e)
+    {
+        var (state, tcs) = ((SendState, TaskCompletionSource))e.UserToken!;
+        state.Dispose();
+
+        if (e.SocketError == SocketError.Success)
+        {
+            tcs.TrySetResult();
+        }
+        else
+        {
+            tcs.TrySetException(new SocketException((int)e.SocketError));
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public void Dispose()
+    {
+        _args.Dispose();
+    }
+
+    class SendState : IDisposable
+    {
+        public MemoryHandle Handle { get; set; }
+
+        public void Dispose()
+        {
+            Handle.Dispose();
+        }
+    }
+}
