c84eff075bbfa1bf130ae860020e70af1a7703d9
Argo Zhang <argo@live.ca>
2025-09-15 09:58:54 +0800

    refactor: 增加 TcpSocketClientOptions 扩展方法

---
+++ src/Longbow.TcpSocket/Extensions/TcpSocketClientOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+namespace Longbow.TcpSocket;
+
+static class TcpSocketClientOptionsExtensions
+{
+    public static void CopyTo(this TcpSocketClientOptions source, TcpSocketClientOptions target)
+    {
+        target.ReceiveBufferSize = source.ReceiveBufferSize;
+        target.IsAutoReceive = source.IsAutoReceive;
+        target.ConnectTimeout = source.ConnectTimeout;
+        target.SendTimeout = source.SendTimeout;
+        target.ReceiveTimeout = source.ReceiveTimeout;
+        target.LocalEndPoint = source.LocalEndPoint;
+        target.IsAutoReconnect = source.IsAutoReconnect;
+        target.ReconnectInterval = source.ReconnectInterval;
+        target.NoDelay = source.NoDelay;
+    }
+}
